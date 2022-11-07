8d9e2e6ed2a9a623aba0d2e9a49456ce0fe9fc9b
NaejBarbosa <emaildojeann@gmail.com>
2022-11-07 09:28:01 -0300

    translate to french

--- nagata.py
+++ nagata.py
@@ -2,5 +2,5 @@ from deep_translator import GoogleTranslator
 to_translate = ['leg', 'arm', 'head', 'cat', 'dog', 'nose', 'egg', 'apple', 'pig', 'hear', 'hair', 'need', 'want']
     
 for i in to_translate:
-    translated = GoogleTranslator(source='english', target='portuguese').translate(i)
+    translated = GoogleTranslator(source='english', target='french').translate(i)
     print(translated)
